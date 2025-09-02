46a1408628bf7171d87103901ba9919e38b2d8b5
Mut3Shad0w <callumgowans0@gmail.com>
2025-09-02 17:54:04 -0400

    updated script to count characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -7,6 +7,13 @@ def get_book_text(book_path):
 def main():
     book_contents = get_book_text("books/frankenstein.txt")
     num_words = count_words(book_contents)
+    num_letter = count_characters(book_contents)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"{num_words} words found in the document")
+    print("--------- Character Count -------")
+    print(f"{num_letter}")
+
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,16 @@ def count_words(book):
     for work in book:
         word_count += 1
     return word_count
+
+def count_characters(book):
+    book = book.lower()
+    #book = book.split()
+    character_dic = {}
+    for letter in book:
+        if letter in character_dic:
+            character_dic[letter] += 1
+        else:
+            character_dic[letter] = 1
+
+
+    return character_dic
