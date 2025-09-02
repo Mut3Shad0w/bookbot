3772c61052a8d711b48b302275f990c72e8deafc
Mut3Shad0w <callumgowans0@gmail.com>
2025-09-02 17:13:17 -0400

    split into two scripts

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
+from stats import count_words
+
 def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
-
-def count_words(book):
-    book = book.split()
-    word_count = 0
-    for work in book:
-        word_count += 1
-    return word_count
 
 def main():
     book_contents = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,6 @@
+def count_words(book):
+    book = book.split()
+    word_count = 0
+    for work in book:
+        word_count += 1
+    return word_count
