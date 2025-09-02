a4223480fab51496991362baf39d02a4d640a25a
Mut3Shad0w <callumgowans0@gmail.com>
2025-09-02 17:06:29 -0400

    updated main.py to print out content of book

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+
+def main():
+    book_contents = get_book_text("books/frankenstein.txt")
+
+    print(book_contents)
+
+main()
